4bf2332c533be9a9829f80e913f88d8cec7d7c29
Gurukrishnaa <128882125+Gurukrishnaa@users.noreply.github.com>
2025-02-17 20:13:13 +0530

    Forntend

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Pulmo-Track</h1>
+        <p>Your AI-powered medical report analysis system.</p>
+      </div>
+    );
+  }
+  
