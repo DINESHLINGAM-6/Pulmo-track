5eda1a63789954d5023607cbbb22c9dc024fcef5
Gurukrishnaa <128882125+Gurukrishnaa@users.noreply.github.com>
2025-02-17 20:33:49 +0530

    Frontend

--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Pulmo-Track</h1>
-        <p>Your AI-powered medical report analysis system.</p>
-      </div>
-    );
-  }
-  
